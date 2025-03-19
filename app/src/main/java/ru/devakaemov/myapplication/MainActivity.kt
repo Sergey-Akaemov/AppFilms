2206f2f5f74641c93f57491aeffe47fa95182fb9
Sergey Akaemov <sergeyakaemov@gmail.com>
2025-03-19 12:11:45 +0300

    filmapp 2

--- app/src/main/java/ru/devakaemov/myapplication/MainActivity.kt
+++ app/src/main/java/ru/devakaemov/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package ru.devakaemov.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_menu).setOnClickListener {
+            Toast.makeText(this, "МЕНЮ", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<Button>(R.id.button_favorites).setOnClickListener {
+            Toast.makeText(this, "ИЗБРАННОЕ", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<Button>(R.id.button_watch_later).setOnClickListener {
+            Toast.makeText(this, "ПОСМОТРЕТЬ ПОЗЖЕ", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<Button>(R.id.button_collection).setOnClickListener {
+            Toast.makeText(this, "ПОДБОРКИ", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<Button>(R.id.button_settings).setOnClickListener {
+            Toast.makeText(this, "НАСТРОЙКИ", Toast.LENGTH_SHORT).show()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
